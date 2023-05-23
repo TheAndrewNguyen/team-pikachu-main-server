6efd5119d9cdb1ed51379a0e917e5f7a35854dd3
TheAndrewNguyen <andrewbvietnguyen@gmail.com>
2023-05-23 07:53:31 -0700

    patch to attempt to fix dependecy issue concerning puppeet

--- routes/algorithm.js
+++ routes/algorithm.js
@@ -77,7 +77,7 @@ async function webScraper(productSearch) {
     //console.log("no product name");
     return null;
   }
-  const browser = await puppeteer.launch({ headless: 'new' });
+  const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox'] });
   const page = await browser.newPage();
   const itemNames = [];
   itemNames.push(productSearch);
